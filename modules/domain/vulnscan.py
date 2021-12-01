098db6188632b6838bb0ce644d63a889f913c521
Syam <syamsv2020@gmail.com>
2021-12-01 15:22:15 +0530

    Delete modules directory

--- modules/domain/vulnscan.py
+++
@@ -1,9 +0,0 @@
-from modules.domain.vscan.sql import scan_sql_injection
-from modules.domain.vscan.vuln import ClickJacking,HostHeader
-from extra.for_inp import formaturl
-
-def init_vulnscan(x):
-    x = formaturl(x)
-    ClickJacking(x)
-    HostHeader(x)
-    scan_sql_injection(str(formaturl(x)+"/"))
